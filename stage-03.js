f3cb0f7216495ebca271b8d948a63cee0a8bd309
eucarizan <eucarizan@gmail.com>
2024-12-11 07:27:45 +0800

    stage 3 done

--- stage-03.js
+++ stage-03.js
@@ -2,8 +2,9 @@ const input = require('sync-input');
 let name = input("Enter name: ");
 let surname = input("Enter surname: ");
 let message = input("Enter message: ");
+let repeats = input("Enter number of repeats: ");
 let output = `This is ${name} ${surname} and ${message}`;
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < repeats; i++) {
   console.log(output);
 }
